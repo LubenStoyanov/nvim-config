8f7df0b5ba952736490f97dc09ae4f61c549dc95
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2023-09-28 12:23:23 +0200

    Initial commit

---
+++ lua/luben/set.lua
@@ -0,0 +1,36 @@
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.opt.guicursor = ''
+
+vim.wo.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
+vim.opt.undofile = true
+
+-- Set highlight on search
+vim.o.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.hi = 0
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append '@-@'
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = '80'
